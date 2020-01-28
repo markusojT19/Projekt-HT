c8931f534714e0c8c448cc7d5443c2e552427b92
markusojT19 <markus.olsson@student.tabyenskilda.se>
2020-01-28 13:20:16 +0100

    added button

---
+++ script.js
@@ -0,0 +1,10 @@
+//Button to remove the Riven Lore text//
+
+function myFunction() {
+  var x = document.getElementById("brödtext");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
